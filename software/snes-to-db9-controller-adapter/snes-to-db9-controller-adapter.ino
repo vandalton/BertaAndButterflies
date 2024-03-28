b7a6fb93a808636eb9d027a9bddc6a4253d73da1
vandalton <van.dalton@gmail.com>
2024-03-28 11:12:08 +0100

    Add description to the snes to db9 adapter source file

--- software/snes-to-db9-controller-adapter/snes-to-db9-controller-adapter.ino
+++ software/snes-to-db9-controller-adapter/snes-to-db9-controller-adapter.ino
@@ -1,3 +1,11 @@
+
+// Atari joystick pinout - seen from the front
+// \1 2 3 4 5/
+//  \6 7 8 9/
+//
+// 1 - up, 2 - down, 3 - left, 4 - right, 5 - NC 6 - fire
+// 7 - 5V (HIGH), 8 - GND (LOW), 9 - NC
+
 void setup() {
   // put your setup code here, to run once:
 
