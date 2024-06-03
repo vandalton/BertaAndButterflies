1d4bc1dc6b2db394cb47ffd2b2a219ad559c734b
vandalton <van.dalton@gmail.com>
2024-06-03 23:39:51 +0200

    Update sample program for communicating with Ds1307 clock

--- software/atari2600-game/bb-ds1307.asm
+++ software/atari2600-game/bb-ds1307.asm
@@ -1,15 +1,18 @@
 ReadSecond
  jsr i2c_startwrite
  bcs Ds1307Error
- lda #0
  jsr i2c_txbyte
  jsr i2c_stopwrite
  jsr i2c_startread
  jsr i2c_rxbyte
  sta temp2
+ jsr i2c_rxbyte
+ sta temp3
+ jsr i2c_rxbyte
+ sta temp4
  jsr i2c_stopread
  lda #0
- lda temp2
+ lda temp3
  rts
 
 Ds1307Error
